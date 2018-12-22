5690e4d1ddfd3c5a3e996cfdc188c9f3ca1b9b4f
joebui <dienbuijoe@outlook.com>
2018-12-22 22:39:33 +0700

    write to file

--- desk.go
+++ desk.go
@@ -1,32 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Desk type and functions
 type desk []string
 
 func (d desk) print() {
-  for _, card := range d {
-    fmt.Println(card)
-  }
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+
+func (d desk) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d desk) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
 // Desk type and functions
 
 func newDesk() desk {
-  cards := desk{}
-  cardSuits := []string{"H", "D", "S", "C"}
-  cardValues := []string{"ace", "1", "2", "3"}
+	cards := desk{}
+	cardSuits := []string{"H", "D", "S", "C"}
+	cardValues := []string{"ace", "1", "2", "3"}
 
-  for _, suit := range cardSuits {
-    for _, value := range cardValues {
-      cards = append(cards, value+" of "+suit)
-    }
-  }
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
 
-  return cards
+	return cards
 }
 
 func deal(d desk, handSize int) (desk, desk) {
-  return d[:handSize], d[handSize:]
+	return d[:handSize], d[handSize:]
 }
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  cards := newDesk()
-  hand, remainingCard := deal(cards, 5)
-  hand.print()
-  fmt.Println("----")
-  remainingCard.print()
+	cards := newDesk()
+	// hand, remainingCard := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("----")
+	// remainingCard.print()
+	fmt.Println(cards.toString())
+	cards.saveToFile("test.txt")
 }
