a5f63e5b31caf037b7a42dc7d487eac5e36d6f8e
joebui <dienbuijoe@outlook.com>
2019-02-06 17:21:42 +0700

    add exercises and interface

---
+++ file-io/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Missing argument")
+		os.Exit(1)
+	}
+
+	newFile, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, newFile)
+}
