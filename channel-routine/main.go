4706f68999770d1bc0a5cc7155557f219f7462f8
joebui <dienbuijoe@outlook.com>
2019-02-07 20:16:12 +0700

    convert to space

--- channel-routine/main.go
+++ channel-routine/main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 func main() {
-	links := []string{
-		"http://google.com",
-		"http://github.com",
-		"http://facebook.com",
-	}
+  links := []string{
+    "http://google.com",
+    "http://github.com",
+    "http://facebook.com",
+  }
 
-	channel := make(chan string)
+  channel := make(chan string)
 
-	for _, link := range links {
-		go checkLink(link, channel)
-	}
+  for _, link := range links {
+    go checkLink(link, channel)
+  }
 
-	for l := range channel {
-		// Listen for data.
-		// fmt.Println(<-channel)
-		go checkLink(l, channel)
-	}
+  for l := range channel {
+    // Listen for data.
+    // fmt.Println(<-channel)
+    go checkLink(l, channel)
+  }
 }
 
 func checkLink(l string, c chan string) {
-	_, err := http.Get(l)
-	if err != nil {
-		fmt.Println("Error")
-		c <- l
-		return
-	}
+  _, err := http.Get(l)
+  if err != nil {
+    fmt.Println("Error")
+    c <- l
+    return
+  }
 
-	fmt.Println(l, "works")
-	c <- l
+  fmt.Println(l, "works")
+  c <- l
 }
